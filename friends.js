06517ffade91ea2b5797eaa6a538bfdc26941c3b
Evan Swanson <evan.sundelius@gmail.com>
2022-08-03 17:18:08 -0500

    Add other friend

--- friends.js
+++ friends.js
@@ -1,6 +1,9 @@
-var bff = "Jake"
+var bff = "friend"
+var otherFriend = "Joey"
 if (bff === "Jake") {
   console.log("Best Friends Forever")
+} else if (otherFriend = "Joey") {
+  console.log("You're still ok I guess")
 } else {
   console.log("Fake Friend")
 }
